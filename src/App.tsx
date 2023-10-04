b102e39c0bcee13750f754cc513102baeeaab90c
JKapturkiewicz <kuba.kapek@gmail.com>
2023-10-04 16:05:24 +0200

    Implement usePaniniStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import SplashScreen from "./components/SplashScreen";
+import usePaniniStore from "./stores/usePaniniStore";
 
 function App() {
+  const { paniniStatus } = usePaniniStore();
   return (
     <div className="main">
-      <SplashScreen />
+      {paniniStatus !== "started" && <SplashScreen />
+      
+      }
+      <div style={{zIndex:0, position: "absolute", top: 0, left:0, width:"100vw", height:"100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <h1>started</h1>
+      </div>
     </div>
   );
 }
