63df0e1a8190a4f33ec8e1624ee7b88ffef10fea
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-02 15:30:56 +0100

    Create input elements

---
+++ src/components/Inputs/DropDownInput/DropDownInput.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import styles from "./DropDownInput.module.css";
+import ArrowDown from "../../Icons/ArrowDown";
+
+interface Props {
+  options: Array<string>;
+  value: string;
+  setValue: (value: string) => void;
+  zIndex?: number;
+}
+
+function DropDownInput({ options, value, setValue, zIndex = 9 }: Props) {
+  const [showItems, setShowItems] = useState<boolean>(false);
+
+  return (
+    <div className={styles.container} style={{ zIndex: zIndex }} onClick={() => setShowItems(!showItems)}>
+      {value}
+      <div className={`${styles.arrow} ${showItems && styles.rotate}`}>
+        <ArrowDown />
+      </div>
+      <div className={styles.itemsContainer} style={showItems ? { display: "flex" } : { display: "none" }}>
+        {options
+          .filter((option) => option != value)
+          .map((option, idx) => (
+            <div key={idx} className={styles.item} onClick={() => setValue(option)}>
+              {option}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default DropDownInput;
