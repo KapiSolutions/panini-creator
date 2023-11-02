93e59bf27dd9b026dc320773ac26e5a20b41d28a
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-02 15:59:30 +0100

    Change component position on panini start

--- src/components/FormContainer/FormContainer.tsx
+++ src/components/FormContainer/FormContainer.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./FormContainer.module.css";
+import usePaniniStore from "../../stores/usePaniniStore";
 
 type PropsWithChildren = {
   children: React.ReactNode;
 };
 
 const FormContainer: React.FC<PropsWithChildren> = ({ children }) => {
+  const { paniniStatus } = usePaniniStore();
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} style={{ position: paniniStatus === "started" ? "absolute" : "fixed" }}>
       <div className={styles.container}>{children}</div>
     </div>
   );
