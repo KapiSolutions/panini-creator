03b058469a8064d68ea3c3260b92f613067d041b
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-03 09:31:10 +0100

    Create checkbox input components

---
+++ src/components/Inputs/CheckBoxRadialInput/CheckBoxRadialItem/CheckBoxRadialItem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./CheckBoxRadialItem.module.css";
+
+interface Props {
+  option: string;
+  value: string;
+  onClick: () => void;
+}
+function CheckBoxSquareItem({ option, value, onClick }: Props): React.ReactElement {
+  return (
+    <div className={styles.option} onClick={() => onClick()}>
+      <div className={`${styles.checkbox} ${value === option && styles.checked}`} />
+      <span>{option}</span>
+    </div>
+  );
+}
+
+export default CheckBoxSquareItem;
---
+++ src/components/Inputs/CheckBoxSquareInput/CheckBoxSquareInput.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./CheckBoxSquareInput.module.css";
+import CheckBoxSquareItem from "./CheckBoxSquareItem/CheckBoxSquareItem";
+
+interface Props {
+  options: Array<string>;
+  value: Array<string>;
+  setValue: (value: Array<string>) => void;
+}
+function CheckBoxSquareInput({ options, value, setValue }: Props): React.ReactElement {
+  const updateValue = (selectedOption: string) => {
+    if (value.includes(selectedOption)) {
+      setValue(value.filter((item) => item !== selectedOption));
+    } else {
+      const tmpArr = [...value];
+      tmpArr.push(selectedOption);
+      setValue(tmpArr);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      {options.map((option, idx) => (
+        <CheckBoxSquareItem key={idx} option={option} value={Array.isArray(value) ? value.join() : value} onClick={() => updateValue(option)} />
+      ))}
+    </div>
+  );
+}
+
+export default CheckBoxSquareInput;
