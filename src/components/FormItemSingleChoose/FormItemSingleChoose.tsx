38651ee0c8dbae2f14fc3a405f144d3795e32f99
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-03 09:36:17 +0100

    Update component props and use default value

--- src/components/FormItemSingleChoose/FormItemSingleChoose.tsx
+++ src/components/FormItemSingleChoose/FormItemSingleChoose.tsx
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 import styles from "./FormItemSingleChoose.module.css";
 import CarouselInput from "../Inputs/CarouselInput/CarouselInput";
+import CheckBoxRadialInput from "../Inputs/CheckBoxRadialInput/CheckBoxRadialInput";
+import CheckBoxSquareItem from "../Inputs/CheckBoxSquareInput/CheckBoxSquareItem/CheckBoxSquareItem";
 
 interface Props {
   title: string;
   options: Array<string>;
+  defaultValue: string | null | boolean;
   type: "carousel" | "radialSelect" | "squareSelect" | "squareSelectString";
   align?: "center" | "start";
 }
 
-function FormItem({ title, options, type, align = "center" }: Props) {
-  const [value, setValue] = useState<string | null>(options[0]);
-
+function FormItemSingleChoose({ title, options, type, defaultValue, align = "center" }: Props) {
+  const [value, setValue] = useState<string | null | boolean>(defaultValue);
+console.log(title,value)
   const getInputField = () => {
     switch (type) {
       case "carousel":
-        return <CarouselInput options={options} value={value} setValue={setValue} />;
+        return <CarouselInput options={options} value={value as string} setValue={setValue} />;
+      case "radialSelect":
+        return <CheckBoxRadialInput options={options} value={value as string} setValue={setValue} />;
+      case "squareSelect":
+        return <CheckBoxSquareItem option={options[0]} value={value as string} onClick={() => setValue(value ? null : options[0])} />;
       default:
         return "Input type is not supported";
     }
@@ -30,4 +37,4 @@ function FormItem({ title, options, type, align = "center" }: Props) {
   );
 }
 
-export default FormItem;
+export default FormItemSingleChoose;
