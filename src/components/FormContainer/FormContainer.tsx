cdcdd6d03c720d09d4d4ee993ebf086b181c4bcd
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-03 14:06:08 +0100

    Update position handling

--- src/components/FormContainer/FormContainer.tsx
+++ src/components/FormContainer/FormContainer.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./FormContainer.module.css";
 import usePaniniStore from "../../stores/usePaniniStore";
+import { config } from "../../config/config";
 
 type PropsWithChildren = {
   children: React.ReactNode;
@@ -8,8 +9,28 @@ type PropsWithChildren = {
 
 const FormContainer: React.FC<PropsWithChildren> = ({ children }) => {
   const { paniniStatus } = usePaniniStore();
+  const [position, setPosition] = useState<"fixed" | "absolute">("fixed");
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    if (paniniStatus === "started") {
+      setPosition("absolute");
+    }
+    timeout = setTimeout(() => {
+      if (paniniStatus === "completed") {
+        setPosition("fixed");
+      }
+    }, config.animationTime + 100);
+
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [paniniStatus]);
   return (
-    <div className={styles.wrapper} style={{ position: paniniStatus === "started" ? "absolute" : "fixed" }}>
+    <div id="header" className={styles.wrapper} style={{ position: position }}>
       <div className={styles.container}>{children}</div>
     </div>
   );
