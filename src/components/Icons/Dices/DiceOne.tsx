95ba0b4cbfb553a4f6ed115c4be2ca32b9b71ce3
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-07 18:12:50 +0100

    Add icons

---
+++ src/components/Icons/Dices/DiceOne.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function DiceOne() {
+  return (
+    <svg width="15" height="17" viewBox="0 0 15 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        x="0.306186"
+        y="10.0088"
+        width="6.69963"
+        height="6.69963"
+        transform="rotate(-15 0.306186 10.0088)"
+        fill="white"
+        stroke="black"
+        strokeWidth="0.5"
+      />
+      <circle cx="4.5" cy="12.5" r="0.5" fill="black" />
+    </svg>
+  );
+}
+
+export default DiceOne;
