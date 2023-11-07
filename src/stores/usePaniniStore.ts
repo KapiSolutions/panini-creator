c52e4380d26c452646639813904eb69bb97decb8
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-07 16:27:24 +0100

    Implement form reset

--- src/stores/usePaniniStore.ts
+++ src/stores/usePaniniStore.ts
@@ -5,15 +5,19 @@ type Statuses = "init" | "started" | "completed";
 interface PaniniStore {
   paniniStatus: Statuses;
   errors: z.ZodIssue[];
+  reset: boolean;
   setPaniniStatus: (value: Statuses) => void;
   setErrors: (value: z.ZodIssue[] | []) => void;
+  setReset: () => void;
 }
 
 const usePaniniStore = create<PaniniStore>((set) => ({
   paniniStatus: "init",
   errors: [],
+  reset: false,
   setPaniniStatus: (value) => set({ paniniStatus: value }),
   setErrors: (value) => set({ errors: value }),
+  setReset: () => set((state) => ({reset: !state.reset}))
 }));
 
 export default usePaniniStore;
