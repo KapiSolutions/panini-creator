cd2719b8150919c634edb46a59a54d6654ff369b
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-07 18:13:22 +0100

    Implement panini randomization

--- src/components/FormHeader/FormHeader.tsx
+++ src/components/FormHeader/FormHeader.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import styles from "./FormHeader.module.css";
-import Dice from "../Icons/Dice";
+import { useFormContext } from "react-hook-form";
+import usePaniniStore from "../../stores/usePaniniStore";
+import { randomPanini } from "../../schema/paniniSchema";
+import DiceOne from "../Icons/Dices/DiceOne";
+import DiceTwo from "../Icons/Dices/DiceTwo";
 
-function FormHeader() {
+function FormHeader(): React.ReactElement {
+  const { reset } = useFormContext();
+  const { setReset } = usePaniniStore();
+
+  const handleRandomize = () => {
+    reset(randomPanini());
+    setReset();
+  };
+
   return (
     <div className={styles.container}>
       <h1>Panini Creator</h1>
       <button>
-        <Dice />
-        <span>Randomize Panini</span>
+        <div className={styles.iconsContainer}>
+          <span className={styles.diceOne}>
+            <DiceOne />
+          </span>
+          <span className={styles.diceTwo}>
+            <DiceTwo />
+          </span>
+        </div>
+        <span onClick={handleRandomize}>Randomize Panini</span>
       </button>
     </div>
   );
