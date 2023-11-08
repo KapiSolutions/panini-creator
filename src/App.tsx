834152655307eca594fd11562be0939bf69c0242
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-08 10:23:16 +0100

    Update components path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import SplashScreen from "./components/SplashScreen/SplashScreen";
 import usePaniniStore from "./stores/usePaniniStore";
-import FormContainer from "./components/FormContainer/FormContainer";
-import FormHeader from "./components/FormHeader/FormHeader";
-import BaseForm from "./components/BaseForm/BaseForm";
-import ExtrasForm from "./components/ExtrasForm/ExtrasForm";
-import FinalizeForm from "./components/FinalizeForm/FinalizeForm";
+import FormContainer from "./components/PaniniForm/FormContainer/FormContainer";
+import FormHeader from "./components/PaniniForm/FormHeader/FormHeader";
+import BaseForm from "./components/PaniniForm/BaseForm/BaseForm";
+import ExtrasForm from "./components/PaniniForm/ExtrasForm/ExtrasForm";
+import FinalizeForm from "./components/PaniniForm/FinalizeForm/FinalizeForm";
 
 function App() {
   const { paniniStatus } = usePaniniStore();
