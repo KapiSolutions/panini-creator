c9ea3128cf9d64ff2e484ce098d673eb50bfbcac
JKapturkiewicz <kuba.kapek@gmail.com>
2023-11-09 14:33:08 +0100

    Add objectsEqual and arraysEqual functions

--- tests/utils/helpers.ts
+++ tests/utils/helpers.ts
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react";
 import type { SandwichPayload } from "../../src/types/types";
 
+// Get form data
 export const getFormData = async (): Promise<SandwichPayload> => {
   const getVegetablesValue = async () => {
     const optionsContainer = screen.getByTestId("base.vegetables");
@@ -37,4 +38,43 @@ export const getFormData = async (): Promise<SandwichPayload> => {
       topping: screen.getByTestId("extras.topping-checkbox")?.className.includes("active") ? "SESAME" : null,
     },
   };
+};
+
+// Compare two objects
+export const objectsEqual = (obj1: Object, obj2: Object) => {
+  let objectsAreTheSame = true;
+  for (const key of Object.keys(obj1)) {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+    if (Array.isArray(val1) && Array.isArray(val2)) {
+      if (!arraysEqual(val1, val2)) {
+        objectsAreTheSame = false;
+      }
+    } else if (typeof val1 === "object" && val1 !== null && typeof val2 === "object" && val2 !== null) {
+      if (!objectsEqual(val1, val2)) {
+        objectsAreTheSame = false;
+      }
+    } else if (val1 !== val2) {
+      objectsAreTheSame = false;
+    }
+
+    if (!objectsAreTheSame) {
+      return false;
+    }
+  }
+  return objectsAreTheSame;
+};
+
+// Compare two arrays
+export const arraysEqual = (arr1: Array<string>, arr2: Array<string>): boolean => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
